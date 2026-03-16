04125633a1ac0d892287dbe4fa25c918098185a9
Patrick Wolowicz <patrick@subzero.eu>
2026-03-16 03:16:33 +0100

    prevent redundant load of maplibre's default map style (#139)

--- Sources/MapLibreSwiftUI/MLNMapViewController.swift
+++ Sources/MapLibreSwiftUI/MLNMapViewController.swift
@@ -7,7 +7,21 @@ public protocol MapViewHostViewController: UIViewController {
 }
 
 public final class MLNMapViewController: UIViewController, MapViewHostViewController {
-    var activity: MapActivity = .standard
+    let activity: MapActivity
+    /// The bootstrap style used when constructing the underlying MLNMapView.
+    /// Runtime style changes happen via `mapView.styleURL` from SwiftUI updates.
+    let initialStyleURL: URL
+
+    public init(initialStyleURL: URL, activity: MapActivity = .standard) {
+        self.initialStyleURL = initialStyleURL
+        self.activity = activity
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     @MainActor
     public var mapView: MLNMapView {
@@ -15,7 +29,7 @@ public final class MLNMapViewController: UIViewController, MapViewHostViewContro
     }
 
     override public func loadView() {
-        view = MLNMapView(frame: .zero)
+        view = MLNMapView(frame: .zero, styleURL: initialStyleURL)
         view.tag = activity.rawValue
     }
 }
